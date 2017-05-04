0752f921263d5058b7406223e92b83a4990464f7
Andy <experiencor@gmail.com>
2017-05-04 20:28:54 +0800

    ipython notebook added

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python clarity_classifier.py
-python conciseness_classifier.py
-
-cd submission && zip -r submission.zip *.predict && cd ..
--- utils.py
+++
@@ -1,8 +0,0 @@
-import os
-import numpy as np
-
-def write_submission(filename, predicted_results):
-    if not os.path.exists('submission'):
-        os.makedirs('submission')
-    np.savetxt('submission/' + filename, predicted_results, fmt='%.5f')
-    print(filename + ' updated!')
